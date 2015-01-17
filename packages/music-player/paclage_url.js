a4bbff90f054cdc21a8e58a07e7a10970ca1ed94
G. Willson <greg@biomassiv.es>
2015-01-17 11:39:44 -0700

    Create paclage_url.js
    cloned for use with site buymziki source

---
+++ packages/music-player/paclage_url.js
@@ -0,0 +1,20 @@
+Package.describe({
+name: 'music-player',
+summary: 'A reactive music player with various backends',
+version: '0.2.0',
+git: 'https://github.com/Rayman/meteor-music-player.git'
+});
+Package.onUse(function(api) {
+api.versionsFrom('METEOR@0.9.4');
+api.use("coffeescript", "client");
+api.use("http", "server");
+api.addFiles('server.js', 'server');
+api.addFiles('music-player.coffee', 'client');
+api.addFiles('backend.coffee', 'client');
+api.export('MusicPlayer', ['client']);
+});
+Package.onTest(function(api) {
+api.use('tinytest');
+api.use('music-player');
+api.addFiles('music-player-tests.js');
+});
